0b8e9768f3f5b90051903a21638e1e3e5838aa14
Alexander Uskov <alex.uskov92@gmail.com>
2021-02-16 00:00:25 +0600

    add solution for 06-deep-vue 03-DateInput

--- 06-deep-vue/03-DateInput/components/DateInput.vue
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppInput>
+  <AppInput v-bind="$attrs" :type="type" :value="inputValue" @change="onChange">
     <!-- Так можно передать все слоты в дочерний компонент -->
     <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
       <slot :name="slot" />
@@ -16,8 +16,57 @@ export default {
   components: { AppInput },
 
   props: {
-    // Чтобы value не было в списке $attrs, его можно указать в списке параметров
-    value: {},
+    type: {
+      type: String,
+      default: 'date',
+      validator: function(value) {
+        return ['date', 'time', 'datetime-local'].includes(value);
+      },
+    },
+    valueAsNumber: {
+      type: Number,
+    },
+    valueAsDate: {
+      type: Date,
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  computed: {
+    withSeconds() {
+      const step = this.$attrs['step'];
+      return this.type === 'time' && step && step % 60 !== 0;
+    },
+    isoStringFirstIndex() {
+      return this.type.includes('date') ? 0 : 11;
+    },
+    isoStringLastIndex() {
+      return this.type.includes('time') ? (this.withSeconds ? 19 : 16) : 10;
+    },
+    inputValue() {
+      let date = this.valueAsDate;
+
+      if (this.valueAsNumber) {
+        date = new Date(this.valueAsNumber);
+      }
+
+      if (!date) {
+        return this.value;
+      }
+
+      return date
+        .toISOString()
+        .slice(this.isoStringFirstIndex, this.isoStringLastIndex);
+    },
+  },
+
+  methods: {
+    onChange(event) {
+      this.$emit('update:valueAsNumber', event.target.valueAsNumber);
+      this.$emit('update:valueAsDate', new Date(event.target.valueAsNumber));
+    },
   },
 };
 </script>
